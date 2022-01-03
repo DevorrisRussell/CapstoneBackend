8eb4d12dac7cc53c4a54eb0379ab47181711febb
Devorris Russell <devorris_r@yahoo.com>
2022-01-02 21:23:06 -0500

    fixed schema

--- models/Equipment.js
+++ models/Equipment.js
@@ -1,20 +1,17 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const Joi = require("joi");
+const mongoose = require("mongoose");
 
 const equipmentSchema = new mongoose.Schema({
-    name: { type: String, required: true, minlength: 2, maxlength: 255 },
-    description: { type: String, required: true},
-    list:{
-        type:Array,
-        default:[],
-    },
-    isAvailable: {type: Boolean, default: true },
-   
-    
-    dateModified: {type: Date, default: Date.now },
+  name: { type: String, required: true, minlength: 2, maxlength: 255 },
+  description: { type: String, required: true },
+  list: {
+    type: Array,
+    default: [],
+  },
+  isAvailable: { type: Boolean, default: true },
+
+  dateModified: { type: Date, default: Date.now },
 });
-
-
 
 module.exports.equipmentSchema = equipmentSchema;
 module.exports.Equipment = mongoose.model("Equipment", equipmentSchema);
--- routes/equipments.js
+++ routes/equipments.js
@@ -1,48 +1,45 @@
 const router = require("express").Router();
-const express = require('express');
+const express = require("express");
 const auth = require("../middleware/auth");
 const { Equipment } = require("../models/equipment");
 
-router.get('', async (req, res) => {
-    const equipmentList = await Equipment.find();
-    return res.send(equipmentList);
+router.get("", async (req, res) => {
+  const equipmentList = await Equipment.find();
+  return res.send(equipmentList);
 });
 
-router.post('/:equipment', async (req, res) => {
-    try {
-        const equipment = new Equipment({
-            name: req.body.name,
-            description: req.body.description,
-            
-          });
-        await equipment.save();
+router.post("/", async (req, res) => {
+  try {
+    const equipment = new Equipment({
+      name: req.body.name,
+      description: req.body.description,
+    });
+    await equipment.save();
 
-        return res.send(equipment);
-    } catch (ex) {
-        return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
+    return res.send(equipment);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
 });
 
 router.delete("/:equipmentId", async (req, res) => {
-    const deleteEquipment = await Equipment.findById(req.params.equipmentId)
-    await deleteEquipment.remove();
-    return res.send();
+  const deleteEquipment = await Equipment.findById(req.params.equipmentId);
+  await deleteEquipment.remove();
+  return res.send();
 });
 
-router.put("/:equipmentId/isAvailable", [auth], async (req,res)=>{
-   
-    try{ 
-   const available = await Equipment.findByIdAndUpdate(
-       req.params.equipmentId,
-       { ...req.body },
-       { isAvailable : false  }
-       
-   )
-   } catch (exception){
-       console.log('Internal Server Error', exception);
-   }
-   
+router.put("/:equipmentId/isAvailable", [auth], async (req, res) => {
+  try {
+    console.log("here");
+    console.log(req.params.equipmentId);
+    const available = await Equipment.findByIdAndUpdate(
+      req.params.equipmentId,
+      { isAvailable: false }
+    );
+    res.json(available);
+  } catch (exception) {
+    console.log("Internal Server Error", exception);
+  }
 });
-
 
 module.exports = router;
